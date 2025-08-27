222f524d418ee6103f4bffbffdea2af1d5910bb2
Kolomyess <henrique.kolomyes@gmail.com>
2025-08-27 09:41:18 -0300

    Add Tarefa in App.tsx
    Adição do componente Tarefa no arquivo App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Contador from './pages/contador/Contador'
 import Home from './pages/home/Home'
+import Tarefa from './pages/tarefa/Tarefa'
 
 function App() {
 
@@ -11,6 +12,8 @@ function App() {
         texto='Esse é o componente Home, que recebeu as props (as propriedades)'/>
         <br></br>
         <Contador />
+
+        <Tarefa />
     </>
   )
 }
