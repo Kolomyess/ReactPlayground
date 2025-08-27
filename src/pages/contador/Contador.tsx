c77fee7be78d676e500493a29970340cde25be8d
Kolomyess <henrique.kolomyes@gmail.com>
2025-08-27 09:13:59 -0300

    Hooks Contador
    Criação de funções e useState

--- src/pages/contador/Contador.tsx
+++ src/pages/contador/Contador.tsx
@@ -1,8 +1,18 @@
-﻿
-function Contador() {
-  return (
-    <div>Componente Contador</div>
+﻿import { useState } from "react"
+
+function Contador(){
+  const[valor, setValor] = useState(0);
+
+  function handleClick(){
+    setValor(valor + 1);
+  }
+
+  return(
+    <div>
+      <h2>Componente Contador</h2>
+      <p>O valor atual é: {valor}</p>
+      <button onClick={handleClick}>Adicionar 1</button>
+    </div>
   )
 }
-
 export default Contador
